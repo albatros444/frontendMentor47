93479a3b7f68e82ea57e6c3af9565b480d1db8b1
alex444 <alexbatenkin@bk.ru>
2024-09-27 01:01:02 +0400

    vertical line on replies section added. Mobile layout halfway

--- components/renderReply.js
+++ components/renderReply.js
@@ -4,7 +4,8 @@ import editReply from "./editReply.js";
 import { upvoteDownvote } from "./upvoteDownvote.js";
 
 export default function renderReply(reply, comment, data) {
-  const cardsColumn = document.querySelector(".cardsColumn");
+  // const cardsColumn = document.querySelector(".cardsColumn");
+  const repliesColumn = document.querySelector(`.repliesColumn${comment.id}`);
 
   const isOwnReply = reply.user.username === data.currentUser.username;
 
@@ -20,6 +21,23 @@ export default function renderReply(reply, comment, data) {
               <button class="downvote down${
                 reply.id
               }"><svg width="11" height="3" xmlns="http://www.w3.org/2000/svg"><path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z" /></svg></button>
+            </div>
+            <div class="mobileActionButtons">
+            ${
+              !isOwnReply
+                ? `<button class="addReplyBtn addReplyBtn${reply.id}">
+                    <svg width="14" height="13" xmlns="http://www.w3.org/2000/svg"><path d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z" /></svg>
+                    Reply
+                  </button>`
+                : ` <button class="deleteBtn deleteBtn${reply.id}">
+                <svg width="12" height="14" xmlns="http://www.w3.org/2000/svg"><path d="M1.167 12.448c0 .854.7 1.552 1.555 1.552h6.222c.856 0 1.556-.698 1.556-1.552V3.5H1.167v8.948Zm10.5-11.281H8.75L7.773 0h-3.88l-.976 1.167H0v1.166h11.667V1.167Z" /></svg>
+                  Delete
+                </button >
+                <button class="editBtn editBtn${reply.id}">
+                <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg"><path d="M13.479 2.872 11.08.474a1.75 1.75 0 0 0-2.327-.06L.879 8.287a1.75 1.75 0 0 0-.5 1.06l-.375 3.648a.875.875 0 0 0 .875.954h.078l3.65-.333c.399-.04.773-.216 1.058-.499l7.875-7.875a1.68 1.68 0 0 0-.061-2.371Zm-2.975 2.923L8.159 3.449 9.865 1.7l2.389 2.39-1.75 1.706Z" /></svg>
+                  Edit
+                </button>`
+            }
             </div>
           </div>
           <div class="mainSec">
@@ -68,7 +86,7 @@ export default function renderReply(reply, comment, data) {
               </p>
             </div>
           </div>`;
-  cardsColumn.appendChild(nestedCommentDiv);
+  repliesColumn.appendChild(nestedCommentDiv);
 
   ////////upvote downvote and reply/////
 
